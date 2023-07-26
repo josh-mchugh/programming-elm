a1700f4391a5abe6ff8b039ac194fbd242468bbf
Josh McHugh <joshua.mchugh01@gmail.com>
2023-07-26 00:25:19 -0400

    Example of the foundational Elm concepts

--- chapter-1/elm-files/src/Main.elm
+++ chapter-1/elm-files/src/Main.elm
@@ -2,5 +2,26 @@ module Main exposing (main)
 
 import Html exposing (text)
 
+greeting : String
+greeting =
+         "Hello, static Elm!"
+
+meaningOfLife : Int
+meaningOfLife = 42
+
+pi : Float
+pi = 3.14
+
+canChuck : Bool
+canChuck = True
+
+sayHello : String -> String
+sayHello name =
+         "Hello, " ++ name ++ "."
+
+bottlesOf : String -> Int -> String
+bottlesOf contents amount =
+          String.fromInt amount ++ " bottles of " ++ contents ++ " on the wall."
+
 main =
-    text "Hello, Elm!"
+    text (bottlesOf "rum" 99)
