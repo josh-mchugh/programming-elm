b210cfa358155ef4bca0001720b0699bf8780185
Josh McHugh <joshua.mchugh01@gmail.com>
2023-07-28 21:19:08 -0400

    Added ability to add comments to a picture

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -2,68 +2,127 @@ module Picshare exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing
+    ( class, disabled, placeholder, src, type_, value )
+import Html.Events exposing ( onClick, onInput, onSubmit )
 
 baseUrl : String
 baseUrl =
     "http://localhost:5000/"
 
+type alias Model =
+    { url : String
+    , caption : String
+    , liked : Bool
+    , comments : List String
+    , newComment : String
+    }
+
 type Msg
-    = Like
-    | Unlike
+    = ToggleLike
+    | UpdateComment String
+    | SaveComment
 
-update:
-    Msg
-    -> { url : String, caption : String, liked : Bool }
-    -> { url : String, caption : String, liked : Bool }
-update msg model =
-    case msg of
-        Like ->
-            { model | liked = True }
-        Unlike ->
-            { model | liked = False }
-
-initialModel : { url : String, caption : String, liked : Bool }
+initialModel : Model
 initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfing"
     , liked = False
+    , comments = [ " Cowabunga, dude!"]
+    , newComment = ""
     }
 
-viewDetailedPhoto :
-    { url : String, caption : String, liked : Bool }
-    -> Html Msg
-viewDetailedPhoto model =
+update: Msg -> Model -> Model
+update msg model =
+    case msg of
+        ToggleLike ->
+            { model | liked = not model.liked }
+        UpdateComment comment ->
+            { model | newComment = comment }
+        SaveComment ->
+            saveNewComment model
+
+saveNewComment : Model -> Model
+saveNewComment model =
+    let
+        comment =
+            String.trim model.newComment
+    in
+    case comment of
+        "" ->
+            model
+        _ -> { model
+                   | comments = model.comments ++ [ comment ]
+                   , newComment = ""
+             }
+
+viewLoveButton : Model -> Html Msg
+viewLoveButton model =
     let
         buttonClass =
             if model.liked then
                 "fa-heart"
             else
                 "fa-heart-o"
-        msg =
-            if model.liked then
-                Unlike
-            else
-                Like
     in
+    div [ class "like-button" ]
+        [ i
+          [ class "fa fa-2x"
+          , class buttonClass
+          , onClick ToggleLike
+          ]
+          []
+        ]
+
+viewComment : String -> Html Msg
+viewComment comment =
+    li []
+        [ strong [] [ text "Comment: " ]
+        , text (" " ++ comment)
+        ]
+
+viewCommentList : List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        [] ->
+            text ""
+        _ ->
+            div [ class "comments" ]
+                [ ul []
+                     (List.map viewComment comments)
+                ]
+
+viewComments : Model -> Html Msg
+viewComments model =
+    div []
+        [ viewCommentList model.comments
+        , form [ class "new-comment", onSubmit SaveComment ]
+            [ input
+                  [ type_ "text"
+                  , placeholder "Add a comment..."
+                  , value model.newComment
+                  , onInput UpdateComment
+                  ]
+                  []
+            , button
+                  [ disabled (String.isEmpty model.newComment) ]
+                  [ text "Save" ]
+            ]
+        ]
+
+viewDetailedPhoto : Model -> Html Msg
+viewDetailedPhoto model =
     div [ class "detailed-photo" ]
         [ img [ src model.url] []
         , div [ class "photo-info" ]
-            [ div [ class "like-button" ]
-                  [ i
-                        [ class "fa fa-2x"
-                        , class buttonClass
-                        , onClick msg
-                        ]
-                        []
-                  ]
+            [ viewLoveButton model 
             , h2 [ class "caption" ] [ text model.caption ]
+            , viewComments model
             ]
         ]
             
 
-view : { url : String, caption : String, liked : Bool } -> Html Msg
+view : Model -> Html Msg
 view model =
     div []
         [ div [ class "header"] 
@@ -72,7 +131,7 @@ view model =
             [ viewDetailedPhoto model ]
         ]
 
-main : Program() { url : String, caption : String, liked : Bool } Msg
+main : Program () Model Msg
 main =
     Browser.sandbox
         { init = initialModel
