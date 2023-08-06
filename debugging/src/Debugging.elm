d180df14a1a45f0f09d8143e78f44f0ba3f7f595
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-06 10:23:45 -0400

    Worked through example of working with Elm.todo to stub out case functions

--- debugging/src/Debugging.elm
+++ debugging/src/Debugging.elm
@@ -4,9 +4,15 @@ import Html exposing (Html, text)
 import Json.Decode as Json
 import Json.Decode.Pipeline exposing (required)
 
+type Breed
+    = Sheltie
+      | Poodle
+      | Beagle
+
 type alias Dog =
     { name: String
     , age: Int
+    , breed: Breed
     }
 
 dogDecoder : Json.Decoder Dog
@@ -14,13 +20,15 @@ dogDecoder =
     Json.succeed Dog
         |> required "name" Json.string
         |> required "age" Json.int
+        |> required "breed" (Json.string |> Json.andThen decodeBreed)
 
 jsonDog : String
 jsonDog =
     """
      {
        "name": "Trucker",
-       "age": 11
+       "age": 11,
+       "breed": "Poodle"
      }
     """
 
@@ -28,17 +36,41 @@ decodedDog : Result Json.Error Dog
 decodedDog =
     Json.decodeString dogDecoder jsonDog
 
+decodeBreed : String -> Json.Decoder Breed
+decodeBreed breed =
+    case String.toLower breed of
+        "sheltie" ->
+            Json.succeed Sheltie
+        "poodle" ->
+            Json.succeed Poodle
+        "beagle" ->
+            Json.succeed Beagle
+        _ ->
+            Json.fail "Handle other breeds in decodeBreed"
+
+breedToString : Breed -> String
+breedToString breed =
+    case breed of
+        Sheltie ->
+            "Sheltie"
+        Poodle ->
+            "Poodle"
+        Beagle ->
+            "Beagle"
+
 viewDog : Dog -> Html msg
 viewDog dog =
     text <|
         dog.name
-        ++ " is "
-        ++ String.fromInt dog.age
-        ++ " years old."
+            ++ " the "
+            ++ breedToString dog.breed
+            ++ " is "
+            ++ String.fromInt dog.age
+            ++ " years old."
 
 main : Html msg
 main =
-    case Debug.log "decodeDog" decodedDog of
+    case decodedDog of
         Ok dog ->
             viewDog dog
         Err _ ->
