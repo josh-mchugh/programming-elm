fc1630b831aece7a920363cc2b5295167b52ad3e
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-08 00:37:09 -0400

    Adding the ping of the pong

--- ping-pong-ports/src/Main.elm
+++ ping-pong-ports/src/Main.elm
@@ -3,27 +3,43 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 
+
 type alias Model =
-    ()
+    { enabled : Bool
+    , count : Int
+    }
+
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( (), Cmd.none )
+    ( { enabled = True
+      , count = 0
+      }
+    , Cmd.none
+    )
+
 
 type Msg
     = NoOp
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     ( model, Cmd.none )
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+
 view : Model -> Html Msg
 view model =
-    div [] [ text "Hello World" ]
+    div []
+        [ button [] [ text "Ping" ]
+        , div [] [ text (String.fromInt model.count) ]
+        ]
+
 
 main : Program () Model Msg
 main =
