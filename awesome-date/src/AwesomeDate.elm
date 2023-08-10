0beda1b045b1ccec6a2b81ff0d8f63267537e2af
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-10 00:33:09 -0400

    More examples of Elm unit tests

--- awesome-date/src/AwesomeDate.elm
+++ awesome-date/src/AwesomeDate.elm
@@ -1,4 +1,4 @@
-module AwesomeDate exposing (Date, create, year)
+module AwesomeDate exposing (Date, create, year, isLeapYear)
 
 type Date
     = Date { year: Int, month: Int, day: Int }
@@ -10,3 +10,11 @@ create year_ month_ day_ =
 year : Date -> Int
 year (Date date) =
     date.year
+
+isLeapYear : Int -> Bool
+isLeapYear year_ =
+    let
+        isDivisibleBy n =
+            remainderBy n year_ == 0
+    in
+    isDivisibleBy 4 && not (isDivisibleBy 100) || isDivisibleBy 400
--- awesome-date/src/AwesomeDateTest.elm
+++ awesome-date/src/AwesomeDateTest.elm
@@ -11,9 +11,36 @@ exampleDate =
 suite : Test
 suite =
     describe "AwesomeDate"
+        [ testDateParts
+        , testIsLeapYear
+        ]
+
+testDateParts : Test
+testDateParts =
+    describe "date part getters"
         [ test "retrieves the year from a date" <|
             \_ ->
                 Date.year exampleDate
                     |> Expect.equal 2012
-            
+        ]
+
+testIsLeapYear : Test
+testIsLeapYear =
+    describe "isLeapYear"
+        [ test "returns true if divisible by 4 but not 100" <|
+            \_ ->
+                Date.isLeapYear 2012
+                    |> Expect.equal True
+        , test "returns false if not divisible by 4" <|
+            \_ ->
+                Date.isLeapYear 2010
+                    |> Expect.equal False
+        , test "returns false if divisible by 4 and 100 but not 400" <|
+            \_ ->
+                Date.isLeapYear 3000
+                    |> Expect.equal False
+        , test "returns true if divisible by 4, 100, and 400" <|
+            \_ ->
+                Date.isLeapYear 2000
+                    |> Expect.equal True
         ]
